90761af72f88833d0d202c9382ca4e4709fb2ea1
Luke Zeches <zechesl@gmail.com>
2024-01-15 15:39:33 -0500

    Improved variable naming and calling script in correct dir

--- launch.sh
+++ launch.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Updates the local repository to the remote on github, once an internet connection has been established
+REPO_DIR=$( cd -- $(dirname -- ${BASH_SOURCE[0]}) &> /dev/null && pwd )
 while true; do
   PING_OUT=$(ping -c 1 github.com)
   if [[ $PING_OUT == *"1 packets transmitted, 1 received"* ]]; then
+    cd -- $REPO_DIR
     git pull > launch_out.txt 2> launch_err.txt
-    python ./main.py
+    python main.py
     break;
   else
     sleep 1

c1eb14441a66c6430afbee1641f4e89a9f2cf712
Luke Zeches <zechesl@gmail.com>
2024-01-15 13:14:11 -0500

    Remove redundant git pull

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@
 while true; do
   PING_OUT=$(ping -c 1 github.com)
   if [[ $PING_OUT == *"1 packets transmitted, 1 received"* ]]; then
-    git fetch > launch_out.txt 2> launch_err.txt && git pull > launch_out.txt 2> launch_err.txt
+    git pull > launch_out.txt 2> launch_err.txt
     python ./main.py
     break;
   else
